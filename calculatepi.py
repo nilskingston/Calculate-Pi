28ee86148493734a0c0556fb62715bef41f128c6
nilskingston <nilskingston@hanovernorwichschools.org>
2015-09-14 14:16:21 -0400

    Updated from Brython Server: 9/14/2015 2:16:22 PM

--- calculatepi.py
+++ calculatepi.py
@@ -26,5 +26,5 @@ n = int(input("How many terms should I use? "))
 decimal = int(input("How many decimal places should I use in the result? "))
 
 p = 4 * sum([((-1.0)**k)/(2 * k + 1) for k in range(0,n)])
+print("The approximate value of pi is {0:.{1}f}".format(p, decimal))
 
-print(p)
